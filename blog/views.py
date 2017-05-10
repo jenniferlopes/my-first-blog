a5814ef88a07054c3cf89e68a161ed4dcbc5a35b
jenniferlopes <jlsilva7@ucs.br>
2017-05-10 08:55:25 -0300

    Inclusão de templates na pasta blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
