2c9b33d196078571d1e28f90a64fc0f225f00b45
jenniferlopes <jlsilva7@ucs.br>
2017-05-10 16:33:20 -0300

    inserção post new na view

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,13 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form}))
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -23,5 +28,5 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
