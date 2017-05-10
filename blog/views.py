a6b425a857a49853eb5323c225abb4927a7e4609
jenniferlopes <jlsilva7@ucs.br>
2017-05-10 16:44:35 -0300

    again

--- blog/views.py
+++ blog/views.py
@@ -27,6 +27,6 @@ def post_edit(request, pk):
 
 def post_new(request):
     form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form}))
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
